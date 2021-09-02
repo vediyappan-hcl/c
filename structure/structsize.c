f4c3d893dd90c47153d464218d21447c07b4beab
vediyappan-villali <vediyappan.villali@gmail.com>
2021-09-02 07:39:22 +0000

    find structure size

--- structure/structsize.c
+++ structure/structsize.c
@@ -1,6 +1,8 @@
 // c program to find the size of the structure
 
 #include <stdio.h>
+
+// defining structure
 struct st
 {
     int     i;
@@ -9,7 +11,10 @@ struct st
 };
 int main()
 {
+    // declaring structure variable
     struct  st s1;
+
+    // printing size of structure
     printf("size of structure = %d\n", sizeof(s1));
     return 0;
 }
