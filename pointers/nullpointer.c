a7b998649607c9387a4661a3deb85ecfe215f05c
vediyappan-villali <vediyappan.villali@gmail.com>
2021-09-16 13:59:12 +0530

    null pointer

---
+++ pointers/nullpointer.c
@@ -0,0 +1,13 @@
+// c program for null pointer
+
+#include <stdio.h>
+int main()
+{
+    // declaring null pointer
+    int *ptr = NULL;
+    
+    // dereferencing null pointer
+    printf("%d\n", *ptr);
+
+    return 0;
+}
