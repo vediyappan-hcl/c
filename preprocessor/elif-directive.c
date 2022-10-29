42cf1d325b182c68feeedc66dd2203a047f6e2b1
vediyappan-villali <vediyappan.villali@gmail.com>
2022-10-29 19:44:49 +0530

    add program to understand #elif directive

---
+++ preprocessor/elif-directive.c
@@ -0,0 +1,41 @@
+/*******************************************************************************
+@desc C program to understand the use of #elif directive.
+*******************************************************************************/
+
+// Sytem Headers
+#include <stdio.h>  // printf
+
+// Macro
+#define FLAG 2
+
+/*******************************************************************************
+@desc Main
+*******************************************************************************/
+int main(void)
+{
+    int iNum1 = 4;
+    int iNum2 = 7;
+
+    // The block with non-zero result is compiled
+    #if FLAG == 0
+        printf("FLAG is 0\n");
+        iNum1 = iNum1 + 2;
+        iNum2 = iNum2 + 4;
+    #elif FLAG == 1
+        printf("FLAG is 1\n");
+        iNum1 = iNum1 - 2;
+        iNum2 = iNum2 - 4;
+    #elif FLAG == 2
+        printf("FLAG is 2\n");
+        iNum1 = iNum1 * 2;
+        iNum2 = iNum2 * 4;
+    #else
+        printf("FLAG is greater than 2 or lesser than 0\n");
+        iNum1 = iNum1 / 2;
+        iNum2 = iNum2 / 4;
+    #endif
+
+    printf("Number1 = %d, Number2 = %d\n", iNum1, iNum2);
+
+    return 0;
+}
