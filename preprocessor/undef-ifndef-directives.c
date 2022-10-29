ad4e13ac6379ef57b149dae10bae3ac1a1569638
vediyappan-villali <vediyappan.villali@gmail.com>
2022-10-29 20:24:10 +0530

    add program to understand #undef directive

--- preprocessor/undef-ifndef-directives.c
+++ preprocessor/undef-ifndef-directives.c
@@ -2,7 +2,7 @@
 @desc C program to understand the use of #undef directive.
 *******************************************************************************/
 
-// Sytem Headers
+// System headers
 #include <stdio.h>  // printf
 
 // Macros
