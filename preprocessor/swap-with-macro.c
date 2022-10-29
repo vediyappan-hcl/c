aa40208a7fa19949321bdcb716b7a95f243040b4
vediyappan-villali <vediyappan.villali@gmail.com>
2022-10-29 19:30:55 +0530

    add program to swap two numbers using macro

---
+++ preprocessor/swap-with-macro.c
@@ -0,0 +1,28 @@
+/*******************************************************************************
+@desc C program to swap two numbers of any data type using macro.
+*******************************************************************************/
+
+// Sytem Headers
+#include <stdio.h>  // printf
+
+// Macro
+#define SWAP(dType,x,y) {dType temp; temp=x, x=y, y=temp;} 
+
+/*******************************************************************************
+@desc Main
+*******************************************************************************/
+int main(void)
+{
+    int iNum1 = 7;
+    int iNum2 = 9;
+
+    printf("Before swapping: iNum1 = %d, iNum2 = %d\n", iNum1, iNum2);
+
+    SWAP(int, iNum1, iNum2);
+    // Expanded as {int temp; temp=iNum1, iNum1=iNum2, iNum2=temp;}
+
+    // Result will be 9 and 7 as expected
+    printf("After swapping:  iNum1 = %d, iNum2 = %d\n", iNum1, iNum2);
+
+    return 0;
+}
