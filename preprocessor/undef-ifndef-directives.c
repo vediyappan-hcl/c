f3bf285d671097e6c1759cfec56e99dd6612bc56
vediyappan-villali <vediyappan.villali@gmail.com>
2022-10-29 20:00:47 +0530

    add program to understand #undef directive

---
+++ preprocessor/undef-ifndef-directives.c
@@ -0,0 +1,36 @@
+/*******************************************************************************
+@desc C program to understand the use of #undef directive.
+*******************************************************************************/
+
+// Sytem Headers
+#include <stdio.h>  // printf
+
+// Macros
+#define FLAG
+
+/*******************************************************************************
+@desc Main
+*******************************************************************************/
+int main(void)
+{
+    int iNum1 = 4;
+    int iNum2 = 7;
+
+    // The block is executed if FLAG is defined
+    #ifdef FLAG
+        printf("FLAG is defined\n");
+        iNum1++;
+        iNum2++;
+    #endif
+    #undef FLAG
+    // This block is executed if FLAG is not defined or undefined
+    #ifndef FLAG
+        printf("FLAG is not defined or undefined\n");
+        iNum1--;
+        iNum2--;
+    #endif
+
+    printf("Number1 = %d, Number2 = %d\n", iNum1, iNum2);
+
+    return 0;
+}
