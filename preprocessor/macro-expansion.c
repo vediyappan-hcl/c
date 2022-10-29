913f09097e1f758cc6d82a9fdf967f72860e5b6d
vediyappan-villali <vediyappan.villali@gmail.com>
2022-10-29 19:11:38 +0530

    add program to understand macro expansion

---
+++ preprocessor/macro-expansion.c
@@ -0,0 +1,20 @@
+/*******************************************************************************
+@desc C program to understand macro expansion.
+*******************************************************************************/
+
+// Sytem Headers
+#include <stdio.h>  // printf
+
+// Capital letters are used to name the macros, distinguish it from other variables
+#define MSG "Welcome to the C world!\n"
+
+/*******************************************************************************
+@desc Main
+*******************************************************************************/
+int main(void)
+{
+    // Preprocessor replaces all the occurences of macro with its expansion
+    printf(MSG);
+
+    return 0;
+}
