d10a84d8879c1fef5b51ac929e3c41c9cd089b29
vediyappan-villali <vediyappan.villali@gmail.com>
2022-11-24 18:27:40 +0530

    add program to understand array of function pointers

--- pointers/array-of-function-pointers.c
+++ pointers/array-of-function-pointers.c
@@ -1,80 +1,43 @@
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
- *
- * Filename:    arrayOfFunctionPointers.c
- * Description: C program to illustrate array of function pointers
- * Author:      Vediyappan V.
- * Date:        27-Nov-2021
- *
- * Brief: 
- *  - A function pointer is a pointer that stores the address of the function
- *    and invokes the function whenever required.
- *  - The function pointer array is called as an array of function pointer.
- *  - Syntax: return_type (*ptr_name[arr-size]) (type1, type2, ...);
- *
- * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+// C program to understand array of function pointer
 
-#include <stdio.h>
+// System headers
+#include <stdio.h>  // printf
 
-// function to add
 int add(int num1, int num2)
 {
-    printf("%s: num1 = %d, num2 = %d\n", __func__, num1, num2);
-    return num1+num2;
+    return num1 + num2;
 }
-
-// function to subtract
 int sub(int num1, int num2)
 {
-    printf("%s: num1 = %d, num2 = %d\n", __func__, num1, num2);
-    return num1-num2;
+    return num1 - num2;
 }
-
-// function to multiply
 int mul(int num1, int num2)
 {
-    printf("%s: num1 = %d, num2 = %d\n", __func__, num1, num2);
-    return num1*num2;
+    return num1 * num2;
 }
-
-// function to divide
 int div(int num1, int num2)
 {
-    printf("%s: num1 = %d, num2 = %d\n", __func__, num1, num2);
-
-    // floating point error handling
-    if (num2)
-        return num1/num2;
-    else
-        return 0;
+    return num1 / num2;
 }
 
-int main()
+// Main
+int main(void)
 {
-    // declaring variables
-    int num1 = 10;
-    int num2 = 5;
-    int option;
-    int result;
+    int (*fp[])(int, int) = {add, sub, mul, div};
+    char *operation[] = {"Add", "Subtract", "Multiply", "Divide"};
+    int num1;
+    int num2;
     int i;
 
-    // declaring function pointer
-    int (*fp[4]) (int, int);
+    printf("Enter number1: ");
+    scanf("%d", &num1);
+    printf("Enter number2: ");
+    scanf("%d", &num2);
 
-    // assigning function names to elements of array of function pointers
-    fp[0] = add;
-    fp[1] = sub;
-    fp[2] = mul;
-    fp[3] = div;
+    for(i=0; i<4; i++)
+    {
+        printf("%s: %d\n", operation[i], (*fp[i])(num1, num2));
+    }
 
-    // scanning user option
-    printf("options: 1. add, 2. sub, 3. mul, 4. div\nenter your option: ");
-    scanf("%d", &option);
-
-    // invoking functions using function pointers
-    result = (*fp[option-1])(num1, num2);
-
-    // displaying result
-    printf("result = %d\n", result);
-    
     return 0;
 }
