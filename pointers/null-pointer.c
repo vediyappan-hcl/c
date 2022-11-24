2def52649fc12fcd7fefac020bff3e98b0a9a349
vediyappan-villali <vediyappan.villali@gmail.com>
2022-11-24 14:17:17 +0530

    add program to understand null pointer

--- pointers/null-pointer.c
+++ pointers/null-pointer.c
@@ -1,41 +1,27 @@
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
- *
- * Filename:    nullPointer.c
- * Description: C program to illustrate null pointer
- * Author:      Vediyappan V.
- * Date:        27-Nov-2021
- *
- * Brief: 
- *  - A null pointer is a pointer which points nothing.
- *  - Dereferencing null pointer will create segmentation fault.
- *
- * Uses:
- *  - Used to initialize a pointer when it is not assigned with any valid memory
- *    address yet.
- *  - Used to perform error handling in pointer related code by checking null
- *    pointer. e.g., dereference pointer variable only if it is not a NULL.
- *  - Used to pass a null pointer to a function when we do not want to pass any
- *    valid memory address.
- *
- * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */ 
+// C program to understand NULL pointer
 
-#include <stdio.h>
-#include <stdlib.h>
+// System headers
+#include <stdio.h>  // printf
+#include <stdlib.h> // exit
 
-int main()
+// Main()
+int main(void)
 {
-    // declaring null pointer
+    // NULL pointer gaurantees that it does not point to any valid memory location
     int *ptr = NULL;
 
-    // checking null pointer - commenting this block may cause program crash
+    // Checking NULL pointer
+    // Uncomment this block to avoild program crash i.e., segmentation fault
+    /*
     if (ptr == NULL)
     {
         printf("ptr is NULL\n");
-	exit(1);
+        exit(1);
     }
-    
-    // dereferencing null pointer
-    printf("dereferenced value = %d\n", *ptr);
+    */
+
+    // Dereferencing NULL pointer may cause program crash i.e., segmentation fault
+    printf("Dereferenced value = %d\n", *ptr);
 
     return 0;
 }
